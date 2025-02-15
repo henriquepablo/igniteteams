00466a0feb83d4b4a053f0005c3e5b62a5458fca
henriquepablo <pablo269704@gmail.com>
2025-02-15 16:42:39 -0300

    chore: config structre folders

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import type React from 'react';
+import { Text, View } from 'react-native';
+
+const Groups: React.FC = () => {
+	return (
+		<View>
+			<Text>Groups</Text>
+		</View>
+	);
+};
+
+export default Groups;
