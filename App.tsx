0dacf9c269d3469d2556f8039ec7ff2d2f30bae1
henriquepablo <pablo269704@gmail.com>
2025-02-22 11:28:19 -0300

    chore: config path mapping

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
+import Groups from '@screens/Groups';
 import type React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
-import Groups from './src/screens/Groups';
 
 function App(): React.JSX.Element {
 	return (
