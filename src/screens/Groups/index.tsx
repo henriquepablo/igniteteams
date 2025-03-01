4f198b42745a880a30a48cf91ec190772d081bff
henriquepablo <pablo269704@gmail.com>
2025-03-01 15:51:46 -0300

    feat: create list component GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,28 @@
 import type React from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 import Header from '@components/Header';
 import Highlight from '@components/highlight';
 import GroupCard from '@components/GroupCard';
-
+import { useState } from 'react';
 const Groups: React.FC = () => {
+
+	const [ groups, setGroups ] = useState<string[]>([]);
+	
 	return (
 		<Container>
 			<Header />
 			
-			<Highlight title='Turmas' subtitle='Jogue com  a sua turma'/>
+			<Highlight title='Turmas' subtitle='Jogue com  a sua turma' />
 			
-			<GroupCard title='Galera do Ignite' />
+			<FlatList 
+				data={ groups }
+				renderItem={({ item }) => (
+					<GroupCard title={ item } />
+				)}
+				keyExtractor={ item => item }
+			/>
+
 		</Container>
 	);
 };
