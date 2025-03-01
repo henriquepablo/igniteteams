a35e0b1908ba7bd830ce08053a8b5c6cd0645ee5
henriquepablo <pablo269704@gmail.com>
2025-03-01 16:18:33 -0300

    test: testing husky

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,34 +1,30 @@
+import GroupCard from '@components/GroupCard';
+import Header from '@components/Header';
+import ListEmpty from '@components/ListEmpty';
+import Highlight from '@components/highlight';
 import type React from 'react';
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Container } from './styles';
-import Header from '@components/Header';
-import Highlight from '@components/highlight';
-import GroupCard from '@components/GroupCard';
-import { useState } from 'react';
-import ListEmpty from '@components/ListEmpty';
 
 const Groups: React.FC = () => {
+	const [groups, setGroups] = useState<string[]>([]);
 
-const [ groups, setGroups ] = useState<string[]>([]);
-	
 	return (
 		<Container>
 			<Header />
-			
-			<Highlight title='Turmas' subtitle='Jogue com  a sua turma' />
-			
-			<FlatList 
-				data={ groups }
-				renderItem={({ item }) => (
-					<GroupCard title={ item } />
-				)}
-				keyExtractor={ item => item }
+
+			<Highlight title="Turmas" subtitle="Jogue com  a sua turma" />
+
+			<FlatList
+				data={groups}
+				renderItem={({ item }) => <GroupCard title={item} />}
+				keyExtractor={(item) => item}
 				contentContainerStyle={groups.length === 0 && { flex: 1 }}
 				ListEmptyComponent={
-					<ListEmpty message='Nenhuma turma foi registrada.'/>
+					<ListEmpty message="Nenhuma turma foi registrada." />
 				}
 			/>
-
 		</Container>
 	);
 };
