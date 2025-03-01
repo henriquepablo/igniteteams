67136ec7cf3124b93bbe506b48c917bf2af926a1
henriquepablo <pablo269704@gmail.com>
2025-02-28 21:14:21 -0300

    chore: config theme to application

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import Groups from '@screens/Groups';
 import type React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import theme from './src/theme';
+import { ThemeProvider } from 'styled-components';
 
 function App(): React.JSX.Element {
 	return (
-		<View style={{flex: 1}}>
+		<ThemeProvider theme={{theme}}>
 			<SafeAreaView />
 			<Groups />
-		</View>
+		</ThemeProvider>
 	);
 }
 
