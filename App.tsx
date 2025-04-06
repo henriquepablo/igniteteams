b8bd315404615bae39bba8a5169885f523cfa511
henriquepablo <pablo269704@gmail.com>
2025-04-06 17:01:57 -0300

    style: status bar

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import theme from "@theme/index";
 import { ThemeProvider }  from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
 import Loading from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
 
@@ -10,7 +11,11 @@ export default function App() {
 
     return (
         <ThemeProvider theme={theme}>
-
+            <StatusBar 
+                barStyle="light-content"
+                translucent
+                backgroundColor="transparent"
+            />
             {  
                 fonntsLoaded ? <Groups /> : <Loading />
             }
