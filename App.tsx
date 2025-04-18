e6f16eae5eb10e5ab3b960bd91312050659440b7
henriquepablo <pablo269704@gmail.com>
2025-04-18 08:56:17 -0300

    feat: add screen new group

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider }  from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
 import Loading from "@components/Loading";
 import { SafeAreaView, StatusBar } from "react-native";
+import NewGroup from "@screens/NewGroup";
 
 export default function App() {
 
@@ -18,7 +19,7 @@ export default function App() {
             />
             <SafeAreaView style={{ backgroundColor: theme.COLORS.GRAY_600 }}/>
             {  
-                fonntsLoaded ? <Groups /> : <Loading />
+                fonntsLoaded ? <NewGroup /> : <Loading />
             }
 
         </ThemeProvider>
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,25 @@
+import Header from "@components/Header";
+import { Container, Content, Icon } from "./styles";
+import Highlight from "@components/Highlight";
+import Button from "@components/Button";
+
+export default function NewGroup() {
+    return (
+        <Container>
+            <Header showBackButton/>
+
+            <Content>
+                <Icon />
+                
+                <Highlight 
+                    title="Nova Turma"
+                    subtitle="Crie a turma para adicionar as pessoas"
+                />
+
+                <Button 
+                    title="Criar"
+                />
+            </Content>
+        </Container>
+    )
+}
