0951487dc56221fed10fcd46e27ae1344ab31399
henriquepablo <pablo269704@gmail.com>
2025-04-22 20:36:59 -0300

    feat: add function to register new players in group

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -4,12 +4,15 @@ import Highlight from "@components/Highlight";
 import ButtonIcon from "@components/ButtonIcon";
 import Input from "@components/Input";
 import Filter from "@components/Filter";
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 import { useState } from "react";
 import PlayerCard from "@components/PlayerCard";
 import ListEmpty from "@components/ListEmpty";
 import Button from "@components/Button";
 import { useRoute } from "@react-navigation/native";
+import { AppError } from "@utils/AppError";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroup } from "@storage/player/playersGetByGroup";
 
 type RouteParams = {
     group: string;
@@ -17,11 +20,39 @@ type RouteParams = {
 
 export default function Players() {
 
+    const [newPlayerName, setNewPlayerName] = useState<string>('');
     const [team, setTeam] = useState<string>('Time A');
     const [players, setPlayers] = useState<string[]>([]);
 
     const route = useRoute();
     const { group } = route.params as RouteParams;
+
+    async function handleAddPlayer() {
+        if(newPlayerName.trim().length === 0){
+            return Alert.alert('Nova Pessoa', 'Informe o nome da pessoa para adicionar.');
+        }
+
+        const newPlayer = {
+            name: newPlayerName,
+            team: team
+        }
+
+        try {
+            await playerAddByGroup(newPlayer, group);
+
+            const players = await playersGetByGroup(group);
+            console.log(players);
+        } catch (error) {
+            if(error instanceof AppError) {
+                Alert.alert('Nova Pessoa', error.message);
+            }
+            else {
+                console.log(error);
+                Alert.alert('Nova Pessoa','Não foi possível adicionar');
+            }
+        }
+    }
+
 
     return(
         <Container>
@@ -36,9 +67,10 @@ export default function Players() {
                 <Input 
                     placeholder="Nome da pessoa"
                     autoCorrect={false}
+                    onChangeText={setNewPlayerName}
                 />
 
-                <ButtonIcon icon="add" />
+                <ButtonIcon icon="add" onPress={handleAddPlayer}/>
             </Form>
 
             <HeaderList>
